6c2ff4c53f4d318b71d967ef205fa73991f84750
RodrigoDornelles <rodrigo.dornelles.poa@gmail.com>
2025-07-02 19:22:19 -0300

    feat: add fps controller

--- GlyCore.cpp
+++ GlyCore.cpp
@@ -13,9 +13,49 @@ void GlyCore::init(uint16_t width, uint16_t height, const char *const game_code)
     Serial.println("gly init end");
 }
 
-void GlyCore::update()
+bool GlyCore::update()
 {
+    unsigned long now = micros();
+    unsigned long delta = now - time_last_frame;
 
+    if (delta >= time_frame) {
+        count_frame++;
+        time_delta = delta;
+        time_last_frame = now;
+    }
+
+    if ((now - time_last_frame_1s) >= 1000000) {
+        time_last_frame_1s = now;
+        fps = count_frame;
+        count_frame = 0;
+    }
+
+    return time_last_frame == now;
+}
+
+uint16_t GlyCore::getAvarageFPS()
+{
+    return fps;
+}
+
+uint16_t GlyCore::getInstantFPS()
+{
+    if (time_delta == 0) return 0;
+    return 1000000 / time_delta;
+}
+
+void GlyCore::setFramerate(uint8_t frames_per_second)
+{
+    if (frames_per_second == 0) {
+        time_frame = 0;
+    } else { 
+        time_frame = 1000000 / frames_per_second;
+    }
+}
+
+void GlyCore::setBtnDebounce(uint8_t miliseconds)
+{
+    time_debounce = miliseconds;
 }
 
 void GlyCore::setBtnKeyboard(uint8_t, uint8_t, const char *const)
--- GlyCore.h
+++ GlyCore.h
@@ -15,14 +15,23 @@ public:
         gly_hook_display_lib(displayLib);
     }
     void init(uint16_t, uint16_t, const char *const);
-    void update();
+    bool update();
     void setFramerate(uint8_t);
     void setBtnDebounce(uint8_t);
     void setBtnKeyboard(uint8_t, uint8_t, const char *const);
+    uint16_t getAvarageFPS();
+    uint16_t getInstantFPS();
     //lua_State *const getLua();
 
 private:
-    //lua_State *L;
+    uint16_t fps = 0;
+    uint16_t count_frame = 0;
+    unsigned long time_debounce = 0;
+    unsigned long time_delta = 0;
+    unsigned long time_frame = 0;
+    unsigned long time_last_frame = 0;
+    unsigned long time_last_frame_1s = 0;
+
 };
 
 #endif
