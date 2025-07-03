ca84dbf33a18876cfba070a5c03e6868c6a30c38
RodrigoDornelles <rodrigo.dornelles.poa@gmail.com>
2025-07-03 16:03:10 -0300

    feat: improve adafruit driver

--- GlyAdafruitST7789.cpp
+++ GlyAdafruitST7789.cpp
@@ -1,6 +1,7 @@
 #include "GlyAdafruitST7789.h"
 
 static Adafruit_ST7789 *tft = nullptr;
+static uint16_t current_color = 0;
 
 void gly_hook_display_lib(Adafruit_ST7789 *lib)
 {
@@ -19,53 +20,78 @@ void gly_hook_display_init(uint16_t width, uint16_t height)
     }
 }
 
-void gly_hook_display_rot(uint8_t rot)
+void native_draw_start(void)
 {
-    if (!tft) return;
-}
 
-void native_draw_start(void) {}
-void native_draw_flush(void) {}
+}
+void native_draw_flush(void)
+{
+
+}
 
 void native_draw_color(uint32_t color)
 {
-
+    if (!tft) return;
+#if defined(__BYTE_ORDER__) && (__BYTE_ORDER__ != __ORDER_BIG_ENDIAN__)
+    union { uint32_t value; struct { uint8_t a, b, g, r;};} rgba = { color };
+#else
+    union { uint32_t value; struct { uint8_t r, g, b, a;};} rgba = { color };
+#endif    
+    current_color = tft->color565(rgba.r, rgba.g, rgba.b);
 }
 
-void native_draw_clear(uint32_t)
+void native_draw_clear(uint32_t color)
 {
     if (!tft) return;
-    tft->fillScreen(ST77XX_BLACK);
+
+#if defined(__BYTE_ORDER__) && (__BYTE_ORDER__ != __ORDER_BIG_ENDIAN__)
+    union { uint32_t value; struct { uint8_t a, b, g, r;};} rgba = { color };
+#else
+    union { uint32_t value; struct { uint8_t r, g, b, a;};} rgba = { color };
+#endif    
+    tft->fillScreen(tft->color565(rgba.r, rgba.g, rgba.b));
 }
 
-void native_draw_rect(uint8_t mode, float x, float y, float w, float h)
+void native_draw_rect(uint8_t mode, int16_t x, int16_t y, int16_t w, int16_t h)
 {
     if (!tft) return;
     if (!mode) {
-        tft->fillRect((int16_t)x, (int16_t)y, (int16_t)w, (int16_t)h, tft->color565(255, 255, 255));
+        tft->fillRect(x, y, w, h, current_color);
     } else {
-        tft->drawRect((int16_t)x, (int16_t)y, (int16_t)w, (int16_t)h, tft->color565(255, 255, 255));
+        tft->drawRect(x, y, w, h, current_color);
     }
 }
 
-void native_draw_line(float, float, float, float)
+void native_draw_line(int16_t x1, int16_t y1, int16_t x2, int16_t y2)
 {
-
+    if (!tft) return;
+    tft->drawLine(x1, y1, x2, y2, current_color);
 }
 
-void native_text_print(float, float, const char*)
+void native_text_print(int16_t x, int16_t y, const char* text)
 {
-
+    if (!tft) return;
+    tft->setCursor(x, y);
+    tft->setTextColor(current_color);
+    tft->print(text);
 }
 
-void native_text_mensure(const char*, float*, float*)
+void native_text_mensure(const char* text, int16_t* w, int16_t* h)
 {
+    if (!tft) return;
+    int16_t x, y;
+    uint16_t width, height;
 
+    tft->getTextBounds(text, 0, 0, &x, &y, &width, &height);
+
+    if (w) *w =  width;
+    if (h) *h =  height;
 }
 
-void native_text_font_size(uint8_t)
+void native_text_font_size(uint8_t size)
 {
-
+    if (!tft) return;
+    tft->setTextSize(size/3);
 }
 
 void native_text_font_name(const char*)
@@ -82,7 +108,7 @@ void native_text_font_previous(void)
 
 }
 
-void native_image_draw(float, float, const char*)
+void native_image_draw(int16_t, int16_t, const char*)
 {
 
 }
